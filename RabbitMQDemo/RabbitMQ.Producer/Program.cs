85748d896fe18c77a10d1a6ccb2b9da3d460fddb
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-09-26 12:39:05 +0600

    Sending message to multiple consumer

---
+++ RabbitMQDemo/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Producer;
+using System.Text;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672")
+        };
+        using var connection=factory.CreateConnection();
+        using var channel = connection.CreateModel();
+       QueueProducer.Publish(channel);
+    }
+}
